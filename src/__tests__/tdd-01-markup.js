1bc1341fb966cc90c1079b33473c7735ba40a888
ext_em <edouard.misset@intest.info>
2023-03-13 13:41:57 +0100

    test: refactor

--- src/__tests__/tdd-01-markup.js
+++ src/__tests__/tdd-01-markup.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Editor } from '../post-editor-01-markup'
 
 test('renders a form with title, content, tags, and a submit button', () => {
-  const { getByLabelText, getByText } = render(<Editor />)
-  getByLabelText(/title/i)
-  getByLabelText(/content/i)
-  getByLabelText(/tags/i)
-  getByText(/submit/i)
+  render(<Editor />)
+  expect(screen.getByLabelText(/title/i)).toBeInTheDocument()
+  expect(screen.getByLabelText(/content/i)).toBeInTheDocument()
+  expect(screen.getByLabelText(/tags/i)).toBeInTheDocument()
+  expect(screen.getByText(/submit/i)).toBeInTheDocument()
 })
