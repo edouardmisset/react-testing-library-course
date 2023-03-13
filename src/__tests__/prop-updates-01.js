561b605ebd56e6ebcca6765c00a2619a6f129296
ext_em <edouard.misset@intest.info>
2023-03-13 12:18:42 +0100

    test: props update part 1

--- src/__tests__/prop-updates-01.js
+++ src/__tests__/prop-updates-01.js
@@ -1,16 +1,13 @@
 import * as React from 'react'
 import user from '@testing-library/user-event'
-import { render } from '@testing-library/react'
-import { FavoriteNumber } from '../favorite-number'
+import {render, screen} from '@testing-library/react'
+import {FavoriteNumber} from '../favorite-number'
 
 test('entering an invalid value shows an error message', () => {
-  const { getByLabelText, getByRole, rerender, debug } = render(
-    <FavoriteNumber />,
-  )
-  const input = getByLabelText(/favorite number/i)
+  const {rerender} = render(<FavoriteNumber />)
+  const input = screen.getByLabelText(/favorite number/i)
   user.type(input, '10')
-  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
-  debug()
+  expect(screen.getByRole('alert')).toHaveTextContent(/the number is invalid/i)
   rerender(<FavoriteNumber max={10} />)
-  debug()
+  expect(screen.queryByRole('alert')).not.toBeInTheDocument()
 })
